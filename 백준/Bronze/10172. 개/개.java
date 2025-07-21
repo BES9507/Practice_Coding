0f1154252368f28fe97fa6ec980735fde0764944
BES9507 <66721305+BES9507@users.noreply.github.com>
2025-07-21 13:08:06 +0900

    [Bronze V] Title: 개, Time: 84 ms, Memory: 12960 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.printf("|\\_/|\n");
+		System.out.printf("|q%1sp|%3s/}\n","","");
+		System.out.printf("(%1s0%1s)\"\"\"\\\n","","");
+		System.out.printf("|\"^\"`%4s|\n","");
+		System.out.printf("||_/=\\\\__|");
+	}
+
+}
