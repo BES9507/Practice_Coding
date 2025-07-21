5a79f61e0cb25dd66f93201c686fb523a6fbe5c6
BES9507 <66721305+BES9507@users.noreply.github.com>
2025-07-21 13:07:46 +0900

    [Bronze V] Title: 고양이, Time: 80 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.printf("\\%4s/\\\n","");
+		System.out.printf("%1s)%2s(%1s\')\n","","","");
+		System.out.printf("(%2s/%2s)\n","","");
+		System.out.printf("%1s\\(__)|","");
+	}
+
+}
